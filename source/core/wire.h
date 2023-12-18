a9672301a1d7d025ead55fa86a972937aa971289
ycwn <ycwn@hyperion>
2023-12-18 18:12:07 +0200

    ~ Cleaned up core/wire.

--- source/core/wire.h
+++ source/core/wire.h
@@ -4,6 +4,7 @@
 #define CORE_WIRE_H
 
 
+
 typedef int (wire_fn)(void *data, uint id, uint value);
 
 
@@ -17,9 +18,6 @@ struct wire {
 };
 
 
-#define MKWIRE(f, d, i) { .fn=(wire_fn*)(f), .data=(d), .id=(i) }
-
-
 
 static int wire_nop(void *data, uint id, uint arg) {
 	return -1;
@@ -29,6 +27,11 @@ static inline void wire_init(struct wire *w) {
 	w->fn   = &wire_nop;
 	w->data = NULL;
 	w->id   = 0;
+}
+
+static inline struct wire wire_make(wire_fn *fn, void *data, uint id) {
+	struct wire w = { .fn=fn, .data=data, .id=id };
+	return w;
 }
 
 static inline int wire_act(struct wire *w) {
